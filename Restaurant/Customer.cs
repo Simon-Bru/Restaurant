b9f6d5e31f81c51ce4220136443aa95dcabe8f81
Clément Rocher <clementrocher71@gmail.com>
2018-02-12 15:49:03 +0100

    Customers

---
+++ Restaurant/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Restaurant
+{
+    delegate bool selectDish(Dish dish);
+
+    public class Customer
+    {
+        private string name;
+
+        
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+    }
+}
