15db7f65b63b6c200ff003342702c8934004e710
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-02-12 14:48:41 +0100

    Add restaurant class

--- Restaurant/Dish.cs
+++ Restaurant/Dish.cs
@@ -14,5 +14,35 @@ namespace Restaurant
         private int m_calories;
         private int m_price;
         private Boolean m_vegan;
+
+        public string Name
+        {
+            get => m_name;
+            set => m_name = value;
+        }
+
+        public Course Course
+        {
+            get => m_course;
+            set => m_course = value;
+        }
+
+        public int Calories
+        {
+            get => m_calories;
+            set => m_calories = value;
+        }
+
+        public int Price
+        {
+            get => m_price;
+            set => m_price = value;
+        }
+
+        public bool Vegan
+        {
+            get => m_vegan;
+            set => m_vegan = value;
+        }
     }
 }
---
+++ Restaurant/Restaurant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+    public class Restaurant : IRestaurant
+    {
+        private List<Dish> m_menu;
+
+        public List<Dish> Menu
+        {
+            get => m_menu;
+            set => m_menu = value;
+        }
+
+        public void Open()
+        {
+            // TODO
+            throw new System.NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            // TODO
+            return base.ToString();
+        }
+    }
+}
